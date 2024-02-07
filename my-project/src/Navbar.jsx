b593a5c4d01997f564565d1250fea4579fc6b44a
Adam Blahout <blahout.adam@seznam.cz>
2024-02-07 04:07:22 +0100

    adding Link to img in nav + fix import problem with Profile

--- my-project/src/Navbar.jsx
+++ my-project/src/Navbar.jsx
@@ -20,7 +20,6 @@ const Nav = () => {
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-              {/*<!-- Mobile menu button-->*/}
               <button
                 type="button"
                 onClick={() => setOpen(!open)}
@@ -31,7 +30,7 @@ const Nav = () => {
               >
                 <span className="absolute -inset-0.5"></span>
                 <span className="sr-only">Open main menu</span>
-                {/*<!--Icon when menu is closed. Menu open: "hidden", Menu closed: "close" -->*/}
+
                 <svg
                   className="block h-6 w-6"
                   fill="none"
@@ -46,7 +45,6 @@ const Nav = () => {
                     d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                   />
                 </svg>
-                {/*<!--Icon when menu is open. Menu open: "block", Menu closed: "hidden"-->*/}
                 <svg
                   className="hidden h-6 w-6"
                   fill="none"
@@ -65,16 +63,16 @@ const Nav = () => {
             </div>
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex flex-shrink-0 items-center">
-                <img
-                  className="h-8 w-auto"
-                  src="/assets/SDT_logo_as_plain.png"
-                  alt="Your Company"
-                ></img>
+                <Link to="/">
+                  <img
+                    className="h-8 w-auto"
+                    src="/assets/SDT_logo_as_plain.png"
+                    alt="Your Company"
+                  ></img>
+                </Link>
               </div>
               <div className="hidden sm:ml-6 sm:block">
                 <div className="flex space-x-4">
-                  {/*<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->*/}
-
                   {Links.map((link) => (
                     <Link
                       key={link.name}
@@ -95,7 +93,6 @@ const Nav = () => {
           </div>
         </div>
 
-        {/* Mobile menu, show/hide based on menu state. */}
         <div className="sm:hidden mb-80" id="mobile-menu ">
           <div
             className={`space-y-1 px-2 pb-3 pt-2 ${
